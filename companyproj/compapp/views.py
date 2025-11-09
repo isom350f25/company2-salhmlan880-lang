cb235f20881143101e8556bbed90bc8aed177381
salhmlan880-lang <salhmlan880@gmail.com>
2025-11-09 14:01:30 +0300

    .

--- companyproj/compapp/views.py
+++ companyproj/compapp/views.py
@@ -9,9 +9,6 @@ def employee_list(request):
     return render(request, 'employee_list.html', {'employees': employees})
 
 
-def employee_detail(request, pk):
-    """Show a single employee and their projects."""
-    employee = get_object_or_404(Employee, pk=pk)
-    # access projects via related_name 'projects'
-    projects = employee.projects.all().order_by('start_date')
-    return render(request, 'employee_detail.html', {'employee': employee, 'projects': projects})
+def employee_detail(request, employee_id):
+    employee = Employee.objects.get(id=employee_id)
+    return render(request, 'employee_detail.html', {'employee':employee})
