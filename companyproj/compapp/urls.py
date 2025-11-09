13ae4c294afe9043c640b80a4a5583057a541b57
salhmlan880-lang <salhmlan880@gmail.com>
2025-11-09 14:49:05 +0300

    .

--- companyproj/compapp/urls.py
+++ companyproj/compapp/urls.py
@@ -1,7 +1,8 @@
 from django.urls import path
 from . import views
-
+ 
 urlpatterns = [
     path('employeeslist/', views.employee_list, name='employee_list'),
-    path('employee/<int:pk>/', views.employee_detail, name='employee_detail'),
+    path('employee/<int:employee_id>/', views.employee_detail, name='employee_detail')
 ]
+ 
--- companyproj/compapp/views.py
+++ companyproj/compapp/views.py
@@ -1,14 +1,16 @@
 from django.shortcuts import render
-from .models import Employee
-from django.shortcuts import get_object_or_404
-
+from .models import Employee, Project
+ 
 # Create your views here.
-
+ 
 def employee_list(request):
     employees = Employee.objects.all().order_by('name')
     return render(request, 'employee_list.html', {'employees': employees})
-
-
+ 
 def employee_detail(request, employee_id):
     employee = Employee.objects.get(id=employee_id)
-    return render(request, 'employee_detail.html', {'employee':employee})
+    projects = Project.objects.all()
+    return render(request, 'employee_detail.html', {'employee':employee,'projects':projects})
+ 
+ 
+ 
